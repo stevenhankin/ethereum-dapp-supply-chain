725b6ca12ca873dbb90bc4eaf3d0952c6ea1ca6b
Steven Hankin <steven.hankin@gmail.com>
2019-05-06 22:28:15 +0100

    refactor: switch to ganache cli
    Needed to do this to fix a weird bug where
    events had stopped emitting

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const options = {
     web3: {
         fallback: {
             type: "ws",
-            url: "ws://127.0.0.1:7545",
+            url: "ws://127.0.0.1:8545",
         },
     },
 };
